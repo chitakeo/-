792310689c993b77734c17d10f504a4a90c34733
Sho Yoshida <e175758@ie.u-ryukyu.ac.jp>
2020-02-10 13:10:50 +0900

    second commit

--- edge_color.py
+++ edge_color.py
@@ -3,11 +3,21 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
+#ピースのBGRそれぞれの中央値または、平均値を取りSIFTでマッチング
+#結果としてはピースと全体の写真のそれぞれの撮影時の光の入り方が異なるため期待するような結果は出ない
 
 
 def main():
-    img_piece = cv2.imread("kirinuki.png") # ファイル読み込み
+    img_piece = cv2.imread("001.png") # ファイル読み込み
     img_zentai = cv2.imread("zentai.jpg")
+    
+    # 色による判別時使用
+    # average = 0, median = any
+    ave_med = 0
+    # 画像の中央値または平均値にかける値
+    # low = 0.3 upp = 1.5 くらいがちょうどいいかも 
+    low = 0.3
+    upp = 1.5
 
     #画像の平滑化
     blur_piece = blur(img_piece)
@@ -16,10 +26,52 @@ def main():
     #輪郭の抽出
     edges_piece = make_edge(blur_piece)
     edges_zentai = make_edge(blur_zentai)
+    
+    # 対象範囲を切り出し
+    height, width, channels = img_piece.shape[:3]
+    boxFromX = 0 #対象範囲開始位置 X座標
+    boxFromY = 0 #対象範囲開始位置 Y座標
+    boxToX = width #対象範囲終了位置 X座標
+    boxToY = height #対象範囲終了位置 Y座標
+    # y:y+h, x:x+w　の順で設定
+    imgBox = img_piece[boxFromY: boxToY, boxFromX: boxToX]
+
+    # ピース以外の部分を削除
+    # 切り取り後の画像を使用予定のため,BGRの値が0の場合は削除
+    b_img = imgBox.T[0][imgBox.T[0] > 0]
+    g_img = imgBox.T[1][imgBox.T[1] > 0]
+    r_img = imgBox.T[2][imgBox.T[2] > 0]
+
+    # RGB平均値を出力
+    # flattenで一次元化しmeanで平均を取得 
+    # print(imgBox.T[0])
+    b_flat = b_img.flatten()
+    g_flat = g_img.flatten()
+    r_flat = r_img.flatten()
+   
+   # 平均値を取るか、中央値を取るのか
+    if ave_med  == 0:
+        b = b_flat.mean()
+        g = g_flat.mean()
+        r = r_flat.mean()
+    else:
+        b = np.median(b_flat)
+        g = np.median(g_flat)
+        r = np.median(r_flat)
+        
+    #色抽出の下限と上限を指定
+    b_lower = int(max(0, low * b))
+    g_lower = int(max(0, low * g))
+    r_lower = int(max(0, low * r))
+
+    b_upper = int(min(255, upp * b))
+    g_upper = int(min(255, upp * g))
+    r_upper = int(min(255, upp * r))
+    
 
     # BGRでの色抽出
-    bgrLower = np.array([70,70,70])    # 抽出する色の下限
-    bgrUpper = np.array([255, 255, 255])    # 抽出する色の上限
+    bgrLower = np.array([b_lower, g_lower, r_lower])    # 抽出する色の下限
+    bgrUpper = np.array([b_upper, g_upper, r_upper])    # 抽出する色の上限
     bgrResult_piece = bgrExtraction(img_piece, bgrLower, bgrUpper)
     bgrResult_zentai = bgrExtraction(img_zentai, bgrLower, bgrUpper)
     
@@ -30,7 +82,7 @@ def main():
     
     #SIFTによるマッチング
     #エッジ
-    sift_matching = matching(edges_piece, edges_zentai)
+    #sift_matching = matching(edges_piece, edges_zentai)
     
     #色
     sift_matching = matching(bgrResult_piece, bgrResult_zentai)
@@ -51,9 +103,11 @@ def main():
 
 
 
+
+
 ## 関数定義 ##
 
-# BGRで特定の色を抽出する関数
+# BGRで閾値を設定し抽出する関数
 def bgrExtraction(image, bgrLower, bgrUpper):
     img_mask = cv2.inRange(image, bgrLower, bgrUpper) # BGRからマスクを作成
     result = cv2.bitwise_and(image, image, mask=img_mask) # 元画像とマスクを合成
@@ -73,7 +127,7 @@ def blur(image):
 	result = cv2.blur(gray,ksize = (3,3))
 	return result
 
-
+# SIFTを用いたマッチング
 def matching(image1, image2):
     sift = cv2.xfeatures2d.SIFT_create()
     kp1, des1 = sift.detectAndCompute(image1,None)
