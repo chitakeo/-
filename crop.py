13e94fd5e97130c406fb9bd9e9ee798599d481da
W-Nakasone <psyqhical@icloud.com>
2020-02-10 11:08:35 +0900

    サイズ調整機能追加

--- crop.py
+++ crop.py
@@ -5,8 +5,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def crop():
-
-    IMAGE_PATH = "/Users/e175729/engineering_design_exercise/pict/piece19.JPG"
+    #サイズ調整用変数(倍率)
+    size = 0.5
+    IMAGE_PATH = "/Users/e175729/engineering_design_exercise/pict/piece1.JPG"
     # 画像を読み込む。
     src = cv2.imread(IMAGE_PATH)
     # 2値化する。ここで画像によってうまく調整
@@ -92,6 +93,9 @@ def crop():
         # cv2.imwrite("aaa.png",mask2*255)
 
         img_alpha = cv2.merge(img_bgr+[mask2])#黒枠の透過画像生成
+        height = img_alpha.shape[0]
+        width = img_alpha.shape[1]
+        img_alpha = cv2.resize(img_alpha , (int(width*size), int(height*size)))
 
         cv2.imwrite("crop.png",img_alpha)#出力。透過画像は.png形式でないと出力できない。
         # Setting for display
@@ -161,6 +165,9 @@ def crop():
         # cv2.imwrite("aaa.png",mask2*255)
 
         img_alpha = cv2.merge(img_bgr+[mask2])#黒枠の透過画像生成
+        height = img_alpha.shape[0]
+        width = img_alpha.shape[1]
+        img_alpha = cv2.resize(img_alpha , (int(width*size), int(height*size)))
 
         cv2.imwrite("crop.png",img_alpha)#出力。透過画像は.png形式でないと出力できない。
         # Setting for display
